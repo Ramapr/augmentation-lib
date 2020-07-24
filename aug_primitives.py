26c05b055e9ce241a25d573d1848cbee8c185025
Ramapr <arcabuceroma79@gmail.com>
2020-07-24 14:00:38 +0300

    add feature random/not parameters

--- aug_primitives.py
+++ aug_primitives.py
@@ -8,94 +8,134 @@ photo = cv2.cvtColor(photo, cv2.COLOR_BGR2RGB)
 imshow(photo)
 
 #%%
+
 def rotate(img, rot='rand'):
-  prim_list = {'180':0,'mirr':1, '180+mirr':2}
-  if rot not in prim_list.keys() and rot!='rand':
-    raise Exception("rot must be in ['180','mirr', '180+mirr']")
+  prim_list = ['180', 'mirr', '180mirr']
+  if rot not in prim_list and rot!='rand':
+    raise Exception("rot must be in ['180','mirr', '180mirr'] or 'rand'")
   if rot=='rand':
-    i = np.random.randint(0,len(prim_list))
-  else:
-    i = prim_list[rot]
-  print(i)
-  if i==0: #'180'
+    rot = prim_list[np.random.randint(0, len(prim_list))]
+  if rot == '180':
     return img[::-1,::-1]
-  if i==1: #'mirr'
+  if rot == 'mirr':
     return img[::-1,:]
-  if i==2: #'180+mirr'
+  if rot == '180mirr':
     return img[:,::-1]
-
+   
+#imshow(rotate(photo))
 #%%
-  
-imshow(rotate(photo))
-
-#%%
-  
-def mirror(img):
-  bound = 0.5
-  p = np.random.random()
-  if p > bound:
+ 
+def mirror(img, p='rand'):
+  #bound = 0.5
+  if p == 'rand':
+    p = np.random.randint(0, 2)
+  if p == 0:
     return img[::-1,:] 
   else:
-    return img[:,::-1] 
-  
-#%%
+    return img[:,::-1]   
   
 #imshow(mirror(photo))  
-
 #%%
 
-#%%
-
-def random_gamma(img):
-  gamma = 7
-  k_g = 1.0/ gamma
+def gamma(img, gamma='rand'):
+  if gamma == 'rand':
+    dark_light = np.random.randint(0,2)
+    if dark_light==0:
+      gamma = np.random.uniform(0.4, 0.92)
+    else:
+      gamma = np.random.uniform(1.2, 2)
+  k_g = 1.0 / gamma
   table = (((np.arange(0, 256) / 255.0) ** k_g) * 255).astype("uint8")
-  # apply gamma correction using the lookup table
   return cv2.LUT(img, table)
 
-#%% 
-
 #imshow(random_gamma(photo))
-
 #%%
 
-def blur(img, kernel):
-  kernel_size = 2 * np.random.randint(1, 4, 2) - 1 
-  out = cv2.GaussianBlur(img, tuple(kernel_size), 0)
+#def blur(img, kernel):
+#  kernel_size = 2 * np.random.randint(1, 4, 2) - 1 
+#  out = cv2.GaussianBlur(img, tuple(kernel_size), 0)
+#  return out
+
+def gauss_blur(img, kernel_size='rand'):
+  # имитирует расфокус
+  shape = np.max(img.shape)
+  
+  if kernel_size=='rand':
+    kernel_size = -1 + 2 * np.random.randint(int(np.round(max(shape//150,2),0)),
+                                          int(np.round(max(shape//70,3),0)))
+  #print(kernel_size)
+  kernel_size = (kernel_size,kernel_size)
+  out = cv2.GaussianBlur(img, kernel_size, 0)
   return out
 
 #%%
 
-#imshow(blur(photo))
+def motion_blur(img, degree='rand', angle='rand'):
+  # имитирует смазанное фото со степенью смаза degree и под углом angle 
+  # взято тут и доработанно https://www.programmersought.com/article/70021133946/
+  shape = np.max(img.shape)
+  if degree=='rand':
+    degree = np.random.randint(int(np.round(max(shape//100,2),0)),
+                            int(np.round(max(shape//70,4),0)))
+  if angle=='rand':
+    angle = np.random.randint(0, 365)
+    
+  print(angle, degree)
+  # This generates a matrix of motion blur kernels at any angle. The greater the degree, the higher the blur.
+  M = cv2.getRotationMatrix2D((degree / 2, degree / 2), angle, 1)
+  motion_blur_kernel = np.diag(np.ones(degree))
+  motion_blur_kernel = cv2.warpAffine(motion_blur_kernel, M, (degree, degree))
+
+  motion_blur_kernel = motion_blur_kernel / degree
+  blurred = cv2.filter2D(img, -1, motion_blur_kernel)
+
+  # convert to uint8
+  cv2.normalize(blurred, blurred, 0, 255, cv2.NORM_MINMAX)
+  blurred = np.array(blurred, dtype=np.uint8)
+  return blurred
+  
+
+#%%
+
+#imshow(motion_blur(photo))
   
 #%%
 
-def brightness(img):
-  #k = np.random.uniform(0.3, 1.7)
-  #print(k)
+def brightness(img, br='rand'):
+  val = {'decr': 0, 'incr':1}
+  
   hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV) #convert it to hsv
-  i = np.random.randint(0, 2)
+  if br == 'rand':
+    i = np.random.randint(0, 2)
+  else:
+    if br not in val.keys():
+      raise Exception('br parameter must be in (incr, decr)')
+    i = val[br]    
   if i == 0:
     #decrease k = 0.35-1
     k = np.random.uniform(0.3, 1.)
-    print(k)
+    #print(k)
     hsv[..., 2] = hsv[..., 2] * k
   else:    
     # increase
     k = np.random.randint(1, 50)
-    print(k)
+    #print(k)
     hsv[..., 2] = np.where((255 - hsv[...,2]) < k, 255, hsv[...,2] + k)
   return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
 
 #%%  
   
-imshow(brightness(photo))
+#imshow(brightness(photo, br='incr'))
 
 #%%
-def contrast(img):
-  k = np.random.uniform(0.2, 1.7) # 0-1 decrease 1-2 - increase
-  print(k)
-  return np.clip(np.round(img * k), 0, 255).astype(np.uint8)
+def contrast(img, kont='rand'):
+  if kont == 'rand':
+    kont = np.random.uniform(0.2, 1.6) # 0-1 decrease 1-2 - increase
+  #print(k)
+  if .2 <= kont <= 1.6:
+    return np.clip(np.round(img * kont), 0, 255).astype(np.uint8)
+  else:
+    raise Exception('Value out of bounds')
 
 #%%  
   
