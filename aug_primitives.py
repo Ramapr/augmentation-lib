c0cffd02899041760fb3a37a70b3d9269e5f822b
Ramapr <arcabuceroma79@gmail.com>
2020-07-24 20:13:57 +0300

    upd brightness()

--- aug_primitives.py
+++ aug_primitives.py
@@ -3,9 +3,9 @@ import cv2
 from skimage.io import imshow, imsave
 
 #%%
-photo = cv2.imread('cat.jpg')
-photo = cv2.cvtColor(photo, cv2.COLOR_BGR2RGB)
-imshow(photo)
+#photo = cv2.imread('cat.jpg')
+#photo = cv2.cvtColor(photo, cv2.COLOR_BGR2RGB)
+#imshow(photo)
 
 #%%
 
@@ -51,11 +51,6 @@ def gamma(img, gamma='rand'):
 #imshow(random_gamma(photo))
 #%%
 
-#def blur(img, kernel):
-#  kernel_size = 2 * np.random.randint(1, 4, 2) - 1 
-#  out = cv2.GaussianBlur(img, tuple(kernel_size), 0)
-#  return out
-
 def gauss_blur(img, kernel_size='rand'):
   # имитирует расфокус
   shape = np.max(img.shape)
@@ -100,107 +95,36 @@ def motion_blur(img, degree='rand', angle='rand'):
 #imshow(motion_blur(photo))
   
 #%%
-
-def brightness(img, br='rand'):
-  val = {'decr': 0, 'incr':1}
+#  0.7 - 0.9
+# 7 - 40
+def brightness(img, k='rand'):
+  #val = {'decr': 0, 'incr':1}
   
   hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV) #convert it to hsv
-  if br == 'rand':
-    i = np.random.randint(0, 2)
-  else:
-    if br not in val.keys():
-      raise Exception('br parameter must be in (incr, decr)')
-    i = val[br]    
-  if i == 0:
-    #decrease k = 0.35-1
-    k = np.random.uniform(0.3, 1.)
-    #print(k)
-    hsv[..., 2] = hsv[..., 2] * k
-  else:    
-    # increase
-    k = np.random.randint(1, 50)
-    #print(k)
-    hsv[..., 2] = np.where((255 - hsv[...,2]) < k, 255, hsv[...,2] + k)
-  return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+  if k == 'rand':
+    v = [- np.random.randint(2, 30), np.random.randint(7, 40)]
+    k = v[np.random.randint(0, 2)]
+  h, s, v = cv2.split(hsv)
+  v = cv2.add(v, k)
+  v[v > 255] = 255
+  v[v < 0] = 0
+  out = cv2.cvtColor(cv2.merge((h, s, v)), cv2.COLOR_HSV2RGB)
+  return out
 
 #%%  
-  
-#imshow(brightness(photo, br='incr'))
+
+#imshow(brightness(photo, k=-30))
 
 #%%
 def contrast(img, kont='rand'):
   if kont == 'rand':
-    kont = np.random.uniform(0.2, 1.6) # 0-1 decrease 1-2 - increase
-  #print(k)
-  if .2 <= kont <= 1.6:
-    return np.clip(np.round(img * kont), 0, 255).astype(np.uint8)
-  else:
-    raise Exception('Value out of bounds')
+    kont = np.random.uniform(0.8, 1.2) # 0-1 decrease 1-2 - increase
+  return np.clip(np.round(img * kont), 0, 255).astype(np.uint8)
 
 #%%  
-  
-imshow(contrast(photo))
 
+#imshow(photo)  
+#  
+#imshow(contrast(photo, kont=1.2))
 
 #%%
-    
-#def random_erase(arr, fig_type='square', fill_param='grey', border=10, max_s=1/6):
-#  #fill = 'grey'
-#  img = arr.copy()
-##  fig_type = 'square'
-##  fill_param = 'grey'
-##  border = 10 # 0.05 * w or h
-##  # calc S
-##  k_s = 1 / 6
-#  w, h, _ = img.shape
-#
-#  point_c = np.random.randint(0 + border, 255 - border, 2) # x, y
-#  
-#  fill = {'black': 0, 
-#          'white': 255, 
-#          'grey': 128, 
-#          'mean':int(np.mean(img)), 
-#          'median':int(np.median(img))}
-#  
-#  #        'rndm':np.random.randint(0, 255, (20, 50, 3))}
-#  s_of_figure = max_s * np.random.random()
-#  
-#  if fig_type == 'circle':
-#    #r = int(((w * h * s_of_figure) / np.pi) ** 0.5)
-#    r = int((w * h * s_of_figure ) ** 0.5) // 2    
-#    
-#    ind = np.ones((2*r, 1)) * np.arange(1, 2*r + 1).T
-#    indx = ind #+ point_c[0]
-#    indy = np.rot90(ind)#ind.T #+ point_c[1]
-#    point_in = (indx-r)**2 + (indy-r)**2 <= r**2
-#    
-#    ind_in = np.argwhere(point_in == True)#, 1, 255)
-#    
-#    t_x = indx[ind_in] # + point_c[0]
-#    t_y = indy[ind_in[:]] + point_c[1]
-#    
-#    #ind_in[:] += point_c[0]
-#    #ind_in[:, 1] += point_c[1]
-#    
-#    img[ind_in[:, 0], ind_in[:, 1], :] = 128
-#    
-#    imshow(img)
-#    
-##  labels = np.ones((arr.shape[0], ), dtype=np.int32)
-##  class_ind = (arr[:, 0]-5)**2 + (arr[:, 1] - 8)**2 <= 36
-##  ind = np.where(class_ind == True)[0] 
-##  labels[ind[:]] = 0
-##  return labels
-#    
-#    
-#  if fig_type == 'square':
-#    a = int((w * h * s_of_figure ) ** 0.5) // 2    
-#    #e, b, c, d = point_c[0] - a, point_c[0] + a, point_c[1] - a, point_c[1] + a
-#    if fill_param != 'rndm':
-#      img[point_c[0]-a:point_c[0]+a, point_c[1]-a:point_c[1]+a] = fill[fill_param]
-#    else:
-#      img[point_c[0]-a:point_c[0]+a, point_c[1]-a:point_c[1]+a] = np.random.randint(0, 255, (a*2, a*2, 3)) # shape
-#      #ValueError: could not broadcast input array from shape (86,86,3) into shape (75,86,3)
-#  return img      
-#%% 
-#imshow(random_erase(photo, fill_param='rndm'))
